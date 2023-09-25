08025e528c467b7833922a5aa1c78916094e04fe
hwysoe <hwysoe@gmail.com>
2023-09-25 19:55:17 +0800

    auth-context

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,68 +1,44 @@
-import { createContext, useReducer } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const TasksContext = createContext({
-  tasks: [],
-  addTask: ({ title, startDate, endDate, priority, status }) => {},
-  setTasks: (tasks) => {},
-  deleteExpense: (id) => {},
-  updateExpense: (id, { title, startDate, endDate, priority, status }) => {},
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
 });
 
-function tasksReducer(state, action) {
-  switch (action.type) {
-    case "ADD":
-      return [action.payload, ...state];
-    case "SET":
-      const inverted = action.payload.reverse();
-      return inverted;
-    case "UPDATE":
-      const updatableTaskIndex = state.findIndex(
-        (task) => task.id === action.payload.id
-      );
-      const updatableTask = state[updatableTaskIndex];
-      const updatedItem = { ...updatableTask, ...action.payload.data };
-      const updatedTasks = [...state];
-      updatedTasks[updatableTaskIndex] = updatedItem;
-      return updatedTasks;
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
 
-    case "DELETE":
-      return state.filter((task) => task.id != action.payload);
-
-    default:
-      return state;
-  }
-}
-
-function TasksContextprovider({ children }) {
-  const [tasksState, dispatch] = useReducer(tasksReducer, []);
-
-  function addTask(taskData) {
-    dispatch({ type: "ADD", payload: taskData });
+  function authenticate(token) {
+    setAuthToken(token);
+    // console.log(token);
+    AsyncStorage.setItem("token", token);
   }
 
-  function setTasks(tasks) {
-    dispatch({ type: "SET", payload: tasks });
+  function logout() {
+    setAuthToken(null);
+    AsyncStorage.removeItem("token");
   }
 
-  function deleteTask(id) {
-    dispatch({ type: "DELETE", payload: id });
-  }
-
-  function updateTask(id, taskData) {
-    dispatch({ type: "UPDATE", payload: { id: id, data: taskData } });
+  function getToken() {
+    // console.log(authToken);
+    // console.log(authToken);
+    return authToken;
+    // return AsyncStorage.setItem("userToken", authToken);
   }
 
   const value = {
-    tasks: tasksState,
-    addTask: addTask,
-    setTasks: setTasks,
-    deleteTask: deleteTask,
-    updateTask: updateTask,
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+    getToken: getToken,
   };
 
-  return (
-    <TasksContext.Provider value={value}>{children}</TasksContext.Provider>
-  );
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
-export default TasksContextprovider;
+export default AuthContextProvider;
