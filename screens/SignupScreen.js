70ff6cc4835cb1ab24c239d19a1a9c210a99c926
hwysoe <hwysoe@gmail.com>
2023-09-25 19:51:36 +0800

    test

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,100 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
-import TasksOutput from "../components/TasksOutput/TasksOutput";
-import { TasksContext } from "../store/tasks-context";
-import { fetchTasks } from "../util/http";
+import { useContext, useState } from "react";
+import { Alert } from "react-native";
+
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { AuthContext } from "../store/auth-context";
-import * as Notifications from "expo-notifications";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createUser } from "../util/auth";
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => {
-    return {
-      shouldPlaySound: false,
-      shouldSetBadge: false,
-      shouldShowAlert: True,
-    };
-  },
-});
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-const Upcoming = () => {
-  const [email, setEmail] = useState("");
-  const getData = async () => {
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
     try {
-      const value = await AsyncStorage.getItem("userEmail");
-      setEmail(value);
-      if (value !== null) {
-        console.log("Data retrieved successfully:", value);
-      } else {
-        console.log("Data not found");
-      }
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
-      console.error("Error retrieving data:", error);
-    }
-  };
-
-  async function scheduleNotificationHandler(children) {
-    try {
-      // Check if the notification has already bdeen scheduled
-      const notificationScheduled = await AsyncStorage.getItem(
-        "notificationScheduled"
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later."
       );
-
-      if (!notificationScheduled) {
-        // Schedule the notification
-        Notifications.scheduleNotificationAsync({
-          content: {
-            title: children.title,
-            body:
-              "Your Task Is Ending Soon on " +
-              children.endDate +
-              ". Please Update The Status Of The Task In The App",
-          },
-          trigger: {
-            seconds: 5,
-          },
-        });
-
-        // Mark the notification as scheduled
-        await AsyncStorage.setItem("notificationScheduled", "true");
-      }
-    } catch (error) {
-      console.error("Error scheduling notification:", error);
+      setIsAuthenticating(false);
     }
   }
-  const tasksCtx = useContext(TasksContext);
-  const authCtx = useContext(AuthContext);
-  userID = authCtx.getToken();
 
-  useEffect(() => {
-    getData();
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
 
-    async function getTasks() {
-      const tasks = await fetchTasks();
-      tasksCtx.setTasks(tasks);
-    }
-    getTasks();
-  }, []);
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
 
-  const upcomingTasks = tasksCtx.tasks.filter((task) => {
-    // console.log(task.userID);
-    const startDate = task.startDate.toISOString().split("T")[0];
-    const today = new Date();
-    const todayDateOnly = new Date().toISOString().split("T")[0];
-    // console.log(todayDateOnly);
-
-    const Upcoming = task.status === "Upcoming" && startDate === todayDateOnly;
-
-    if (Upcoming) {
-      console.log(Upcoming);
-      scheduleNotificationHandler(task);
-    }
-
-    return task.status === "Upcoming" && task.email === email;
-  });
-  return <TasksOutput tasks={upcomingTasks} fallbackText="No Upcoming Tasks" />;
-};
-
-export default Upcoming;
-
-const styles = StyleSheet.create({});
+export default SignupScreen;
--- store/auth-context.js
+++ store/auth-context.js
@@ -1,44 +1,68 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useReducer } from "react";
 
-import { createContext, useEffect, useState } from "react";
-
-export const AuthContext = createContext({
-  token: "",
-  isAuthenticated: false,
-  authenticate: (token) => {},
-  logout: () => {},
+export const TasksContext = createContext({
+  tasks: [],
+  addTask: ({ title, startDate, endDate, priority, status }) => {},
+  setTasks: (tasks) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { title, startDate, endDate, priority, status }) => {},
 });
 
-function AuthContextProvider({ children }) {
-  const [authToken, setAuthToken] = useState();
+function tasksReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [action.payload, ...state];
+    case "SET":
+      const inverted = action.payload.reverse();
+      return inverted;
+    case "UPDATE":
+      const updatableTaskIndex = state.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      const updatableTask = state[updatableTaskIndex];
+      const updatedItem = { ...updatableTask, ...action.payload.data };
+      const updatedTasks = [...state];
+      updatedTasks[updatableTaskIndex] = updatedItem;
+      return updatedTasks;
 
-  function authenticate(token) {
-    setAuthToken(token);
-    // console.log(token);
-    AsyncStorage.setItem("token", token);
+    case "DELETE":
+      return state.filter((task) => task.id != action.payload);
+
+    default:
+      return state;
+  }
+}
+
+function TasksContextprovider({ children }) {
+  const [tasksState, dispatch] = useReducer(tasksReducer, []);
+
+  function addTask(taskData) {
+    dispatch({ type: "ADD", payload: taskData });
   }
 
-  function logout() {
-    setAuthToken(null);
-    AsyncStorage.removeItem("token");
+  function setTasks(tasks) {
+    dispatch({ type: "SET", payload: tasks });
   }
 
-  function getToken() {
-    // console.log(authToken);
-    // console.log(authToken);
-    return authToken;
-    // return AsyncStorage.setItem("userToken", authToken);
+  function deleteTask(id) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+
+  function updateTask(id, taskData) {
+    dispatch({ type: "UPDATE", payload: { id: id, data: taskData } });
   }
 
   const value = {
-    token: authToken,
-    isAuthenticated: !!authToken,
-    authenticate: authenticate,
-    logout: logout,
-    getToken: getToken,
+    tasks: tasksState,
+    addTask: addTask,
+    setTasks: setTasks,
+    deleteTask: deleteTask,
+    updateTask: updateTask,
   };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <TasksContext.Provider value={value}>{children}</TasksContext.Provider>
+  );
 }
 
-export default AuthContextProvider;
+export default TasksContextprovider;
